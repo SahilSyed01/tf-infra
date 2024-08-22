fa1e4a316bc51ac78780c51d7b7364727cea8f13
SahilSyed01 <sahilsyed7781@gmail.com>
2024-08-22 12:14:40 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.63.0"
+    }
+  }
+}
 
 # terraform {
 #   backend "s3" {
